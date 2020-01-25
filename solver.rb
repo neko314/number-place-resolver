03601531fc014d53c18bc78e9302ee268864a342
keiko <keiko.cda@gmail.com>
2020-01-25 15:13:52 +0900

    Change how to show numbers

--- solver.rb
+++ solver.rb
@@ -1,30 +1,28 @@
 # 9x9のマス
-# 縦の位置は上から l 0-8
-# 横の位置は左から w 0-8
+# 縦の位置(w)は上から A..I
+# 横の位置(l)は左から 0..8
 # 処理したいマスの位置は[l][w]となる
 # 左上のマスから始まり、左から右へ、上から下へと1マスずつ進む時 l > 9 のときはすべて終了の判定をtrueとする
  
-@l = 0
-@w = 0
+# check_list = (1..9)
 
-def question(n)
-  0.upto(8) do |i|
-    return unless n[i]
+rows = {
+  A: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  B: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  C: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  D: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  E: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  F: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  G: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  H: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+  I: [1, 2, 3, 4, 0, 6, 7, 8, 9 ],
+}
 
-    row = ''
-    n[i].each do |number|
-      row += number.to_s + ' '
-    end
-    puts row
+def show_question(rows)
+  ('A'..'I').each.with_index(1) do |s, i|
+    puts rows[s.to_sym].join (' ')
+    puts '-' * 18 if i % 3 == 0
   end
 end
 
-def solve
-  loop do
-    p @l
-    @l += 1
-    break if @l >= 9
-  end
-end
-
-question([[1, 2, 3, 4, 5, 6, 7, 8,  9],[1, 2, 3, 4, 5, 6, 7, 8,  9]])
+show_question(rows)
